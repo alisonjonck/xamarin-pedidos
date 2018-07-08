a71f2caedb06505e0d2911427d06a2ef0d4e9cc0
Alison <alisonjonck@gmail.com>
2018-07-08 14:42:50 -0300

    add entidade e teste categoria

---
+++ Pedidos/Entities/Categoria.cs
@@ -0,0 +1,14 @@
+﻿namespace Pedidos.Entities
+{
+    public class Categoria
+    {
+        public Categoria(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
