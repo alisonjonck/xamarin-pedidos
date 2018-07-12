67dc1ae6d7bc46b07389c18be16142f27de8d4d2
Alison <alisonjonck@gmail.com>
2018-07-12 09:56:10 -0300

    refactor produto promocao, add GetPrecoPromocao e apply das politicas

--- Pedidos_Domain/Entities/CatalogoPromocao.cs
+++ Pedidos_Domain/Entities/CatalogoPromocao.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Pedidos_Domain.Entities
 {
@@ -7,11 +9,25 @@ namespace Pedidos_Domain.Entities
         public CatalogoPromocao()
         {
             Promocao = new Promocao();
-            Produtos = new List<ProdutoPromocao>();
+            Produtos = new List<Produto>();
         }
 
         public Promocao Promocao { get; set; }
 
-        public List<ProdutoPromocao> Produtos { get; set; }
+        public List<Produto> Produtos { get; set; }
+
+        public decimal GetPrecoPromocao(int quantidade, int produtoId)
+        {
+            var produto = Produtos.FirstOrDefault(p => p.Id == produtoId);
+
+            if (produto.CategoryId != Promocao.CategoryId)
+                throw new Exception($"Produto deve pertencer a categoria Id {Promocao.CategoryId}");
+
+            var politica = Promocao.Policies.OrderByDescending(p => p.Min)
+                                   .FirstOrDefault(p => p.Min <= quantidade);
+
+            return (produto.Price * (politica != null ? (1 - (politica.Discount / 100)) : 1))
+                * quantidade;
+        }
     }
 }
--- Pedidos_Domain_Test/Entities/CatalogoPromocaoTest.cs
+++ Pedidos_Domain_Test/Entities/CatalogoPromocaoTest.cs
@@ -9,15 +9,90 @@ namespace Pedidos_Domain_Test.Entities
     [TestClass]
     public class CatalogoPromocaoTest
     {
+        CatalogoPromocao catalogo;
+
+        [TestInitialize]
+        public void StartUp()
+        {
+            catalogo = new CatalogoPromocao();
+        }
+
         [TestMethod]
         public void TestCatalogoPromocaoInstance()
         {
-            var catalogo = new CatalogoPromocao();
-
             Assert.IsNotNull(catalogo);
 
             Assert.IsNotNull(catalogo.Promocao);
-            Assert.IsInstanceOfType(catalogo.Produtos, typeof(List<ProdutoPromocao>));
+            Assert.IsInstanceOfType(catalogo.Produtos, typeof(List<Produto>));
+        }
+
+        [TestMethod]
+        public void TestCatalogoGetPrecoPromocao()
+        {
+            var politicas = new List<Politica>();
+            politicas.Add(new Politica(5, 10));
+
+            catalogo.Promocao = new Promocao("Teste sem desconto", 1, politicas);
+
+            catalogo.Produtos.Add(new Produto()
+            {
+                Id = 1,
+                Price = 10M,
+                CategoryId = 1
+            });
+
+            decimal precoDesconto = catalogo.GetPrecoPromocao(quantidade: 2, produtoId: 1);
+
+            Assert.IsNotNull(precoDesconto);
+            Assert.AreEqual(20M, precoDesconto);
+        }
+
+        [TestMethod]
+        public void TestCatalogoGetPrecoPromocaoAppliesPolicy()
+        {
+            var politicas = new List<Politica>();
+            politicas.Add(new Politica(5, 10));
+
+            catalogo.Promocao = new Promocao("Teste desconto 10%", 1, politicas);
+
+            catalogo.Produtos.Add(new Produto()
+            {
+                Id = 1,
+                Price = 10M,
+                CategoryId = 1
+            });
+
+            decimal precoDesconto = catalogo.GetPrecoPromocao(quantidade: 5, produtoId: 1);
+
+            Assert.IsNotNull(precoDesconto);
+            Assert.AreEqual(45M, precoDesconto);
+
+            politicas.Add(new Politica(10, 20));
+
+            catalogo.Promocao = new Promocao("Teste desconto 20%", 1, politicas);
+
+            precoDesconto = catalogo.GetPrecoPromocao(quantidade: 10, produtoId: 1);
+
+            Assert.AreEqual(80M, precoDesconto);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestProdutoMustBeAttachedToCategoriaPromocao()
+        {
+            var politicas = new List<Politica>();
+            politicas.Add(new Politica(5, 10));
+
+            catalogo.Promocao = new Promocao("Teste Produto Categoria pertence a promoção", categoryId: 1, policies: politicas);
+
+            catalogo.Produtos.Add(new Produto()
+            {
+                Id = 1,
+                Price = 10M,
+                CategoryId = 2
+            });
+
+            decimal precoDesconto = catalogo.GetPrecoPromocao(quantidade: 1, produtoId: 1);
         }
     }
 }
